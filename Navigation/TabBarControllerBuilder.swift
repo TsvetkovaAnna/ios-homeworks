ca1a20e5286ab3f0d95a31b95c925c2d7c810410
Anna Tsvetkova <Anna@MacBook-Anna.local>
2022-05-10 05:55:44 +0300

    Deleted avatar tabBar element

--- Navigation/TabBarControllerBuilder.swift
+++ Navigation/TabBarControllerBuilder.swift
@@ -6,14 +6,14 @@ class TabBarControllerBuilder {
     static func build() -> UITabBarController {
         let mainTabBarController = UITabBarController()
         let feedNavigationController = UINavigationController(rootViewController: FeedViewController())
-        let avatarNavigationController = UINavigationController(rootViewController: AvatarGestureViewController())
+        //let avatarNavigationController = UINavigationController(rootViewController: AvatarGestureViewController())
         let loginNavigationController = UINavigationController(rootViewController: LogInViewController())
         
         feedNavigationController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper.circle"), tag: 0)
         loginNavigationController.tabBarItem = UITabBarItem(title: "Person", image: UIImage(systemName: "person.circle.fill"), tag: 1)
-        avatarNavigationController.tabBarItem = UITabBarItem(title: "Avatar", image: UIImage(systemName: "pawprint"), tag: 2)
+        //avatarNavigationController.tabBarItem = UITabBarItem(title: "Avatar", image: UIImage(systemName: "pawprint"), tag: 2)
         
-        mainTabBarController.setViewControllers([feedNavigationController, loginNavigationController, avatarNavigationController], animated: false)
+        mainTabBarController.setViewControllers([feedNavigationController, loginNavigationController/*, avatarNavigationController*/], animated: false)
         return mainTabBarController
     }
 }
